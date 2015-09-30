facad5a0ea5acf545164fb51fe25e9de5c44ae53
lliu <liulangliupan@163.com>
2015-09-30 10:14:00 +0800

    Revert "Revert "branch for learn""
    This reverts commit 3494936728ccdf19c3a5ed40c5b3ee82295da95a.

--- Learn.js
+++ Learn.js
@@ -1,5 +1,4 @@
 console.log("Hello");
-<<<<<<< HEAD
 consle.log("Hello again")
 
 console.log("this is the branch for learn")
@@ -7,6 +6,3 @@ console.log("this is the branch for learn")
 console.log("this is for default branch<master")
 
 console.log("test again")
-=======
-consle.log("Hello again")
->>>>>>> parent of d2750a0... branch for learn
